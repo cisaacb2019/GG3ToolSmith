65b6bd6baeffa4e197298e561e2ca9a0985806f7
Collin B <82788579+cisaacb2019@users.noreply.github.com>
2023-06-12 22:57:11 -0600

    Added ui funtionallity and a few additional ui improvments felt nessisary to function properly still would like to run a test

--- GG3GrblProbe/ManifestCreator.Designer.cs
+++ GG3GrblProbe/ManifestCreator.Designer.cs
@@ -28,12 +28,210 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.JobNameInput = new System.Windows.Forms.TextBox();
+            this.jobNameWrite = new System.Windows.Forms.Button();
+            this.jobTextInput = new System.Windows.Forms.TextBox();
+            this.JobTextWrite = new System.Windows.Forms.Button();
+            this.stepNameInput = new System.Windows.Forms.TextBox();
+            this.stepNameWrite = new System.Windows.Forms.Button();
+            this.StepTextInput = new System.Windows.Forms.TextBox();
+            this.StepTextWrite = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.FolderNameText = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.stepGcodeSelect = new System.Windows.Forms.Button();
+            this.WriteStepGcode = new System.Windows.Forms.Button();
+            this.OutputCode = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // JobNameInput
+            // 
+            this.JobNameInput.BackColor = System.Drawing.Color.Aquamarine;
+            this.JobNameInput.Location = new System.Drawing.Point(12, 36);
+            this.JobNameInput.Name = "JobNameInput";
+            this.JobNameInput.Size = new System.Drawing.Size(776, 27);
+            this.JobNameInput.TabIndex = 0;
+            this.JobNameInput.Text = "Enter Job Name:";
+            this.JobNameInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.JobNameInput.Visible = false;
+            // 
+            // jobNameWrite
+            // 
+            this.jobNameWrite.BackColor = System.Drawing.Color.Aquamarine;
+            this.jobNameWrite.Location = new System.Drawing.Point(12, 69);
+            this.jobNameWrite.Name = "jobNameWrite";
+            this.jobNameWrite.Size = new System.Drawing.Size(776, 29);
+            this.jobNameWrite.TabIndex = 1;
+            this.jobNameWrite.Text = "Write:";
+            this.jobNameWrite.UseVisualStyleBackColor = false;
+            this.jobNameWrite.Visible = false;
+            this.jobNameWrite.Click += new System.EventHandler(this.jobNameWrite_Click);
+            // 
+            // jobTextInput
+            // 
+            this.jobTextInput.Location = new System.Drawing.Point(12, 104);
+            this.jobTextInput.Name = "jobTextInput";
+            this.jobTextInput.Size = new System.Drawing.Size(776, 27);
+            this.jobTextInput.TabIndex = 2;
+            this.jobTextInput.Text = "Enter Job Text:";
+            this.jobTextInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.jobTextInput.Visible = false;
+            // 
+            // JobTextWrite
+            // 
+            this.JobTextWrite.Location = new System.Drawing.Point(12, 137);
+            this.JobTextWrite.Name = "JobTextWrite";
+            this.JobTextWrite.Size = new System.Drawing.Size(776, 29);
+            this.JobTextWrite.TabIndex = 3;
+            this.JobTextWrite.Text = "Write:";
+            this.JobTextWrite.UseVisualStyleBackColor = true;
+            this.JobTextWrite.Visible = false;
+            this.JobTextWrite.Click += new System.EventHandler(this.JobTextWrite_Click);
+            // 
+            // stepNameInput
+            // 
+            this.stepNameInput.Location = new System.Drawing.Point(12, 172);
+            this.stepNameInput.Name = "stepNameInput";
+            this.stepNameInput.Size = new System.Drawing.Size(776, 27);
+            this.stepNameInput.TabIndex = 4;
+            this.stepNameInput.Text = "Enter Step Name:";
+            this.stepNameInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.stepNameInput.Visible = false;
+            // 
+            // stepNameWrite
+            // 
+            this.stepNameWrite.Location = new System.Drawing.Point(12, 205);
+            this.stepNameWrite.Name = "stepNameWrite";
+            this.stepNameWrite.Size = new System.Drawing.Size(776, 29);
+            this.stepNameWrite.TabIndex = 5;
+            this.stepNameWrite.Text = "Write:";
+            this.stepNameWrite.UseVisualStyleBackColor = true;
+            this.stepNameWrite.Visible = false;
+            this.stepNameWrite.Click += new System.EventHandler(this.stepNameWrite_Click);
+            // 
+            // StepTextInput
+            // 
+            this.StepTextInput.Location = new System.Drawing.Point(12, 240);
+            this.StepTextInput.Name = "StepTextInput";
+            this.StepTextInput.Size = new System.Drawing.Size(776, 27);
+            this.StepTextInput.TabIndex = 6;
+            this.StepTextInput.Text = "Enter Step Text:";
+            this.StepTextInput.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.StepTextInput.Visible = false;
+            // 
+            // StepTextWrite
+            // 
+            this.StepTextWrite.Location = new System.Drawing.Point(12, 273);
+            this.StepTextWrite.Name = "StepTextWrite";
+            this.StepTextWrite.Size = new System.Drawing.Size(776, 29);
+            this.StepTextWrite.TabIndex = 7;
+            this.StepTextWrite.Text = "Write:";
+            this.StepTextWrite.UseVisualStyleBackColor = true;
+            this.StepTextWrite.Visible = false;
+            this.StepTextWrite.Click += new System.EventHandler(this.StepTextWrite_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button5.Location = new System.Drawing.Point(12, 705);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(776, 85);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Create Save Location:";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // FolderNameText
+            // 
+            this.FolderNameText.Location = new System.Drawing.Point(12, 672);
+            this.FolderNameText.Name = "FolderNameText";
+            this.FolderNameText.Size = new System.Drawing.Size(776, 27);
+            this.FolderNameText.TabIndex = 9;
+            this.FolderNameText.Text = "Enter New Folder Name:";
+            this.FolderNameText.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Segoe UI", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button6.Location = new System.Drawing.Point(12, 581);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(776, 85);
+            this.button6.TabIndex = 10;
+            this.button6.Text = "Select Existing Folder:";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // stepGcodeSelect
+            // 
+            this.stepGcodeSelect.Location = new System.Drawing.Point(12, 308);
+            this.stepGcodeSelect.Name = "stepGcodeSelect";
+            this.stepGcodeSelect.Size = new System.Drawing.Size(776, 29);
+            this.stepGcodeSelect.TabIndex = 11;
+            this.stepGcodeSelect.Text = "Select Step Gcode:";
+            this.stepGcodeSelect.UseVisualStyleBackColor = true;
+            this.stepGcodeSelect.Visible = false;
+            this.stepGcodeSelect.Click += new System.EventHandler(this.stepGcodeSelect_Click);
+            // 
+            // WriteStepGcode
+            // 
+            this.WriteStepGcode.Location = new System.Drawing.Point(12, 343);
+            this.WriteStepGcode.Name = "WriteStepGcode";
+            this.WriteStepGcode.Size = new System.Drawing.Size(776, 29);
+            this.WriteStepGcode.TabIndex = 12;
+            this.WriteStepGcode.Text = "Write:";
+            this.WriteStepGcode.UseVisualStyleBackColor = true;
+            this.WriteStepGcode.Visible = false;
+            this.WriteStepGcode.Click += new System.EventHandler(this.WriteStepGcode_Click);
+            // 
+            // OutputCode
+            // 
+            this.OutputCode.Location = new System.Drawing.Point(12, 406);
+            this.OutputCode.Name = "OutputCode";
+            this.OutputCode.Size = new System.Drawing.Size(776, 169);
+            this.OutputCode.TabIndex = 13;
+            this.OutputCode.Text = "";
+            // 
+            // ManifestCreator
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 1055);
+            this.Controls.Add(this.OutputCode);
+            this.Controls.Add(this.WriteStepGcode);
+            this.Controls.Add(this.stepGcodeSelect);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.FolderNameText);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.StepTextWrite);
+            this.Controls.Add(this.StepTextInput);
+            this.Controls.Add(this.stepNameWrite);
+            this.Controls.Add(this.stepNameInput);
+            this.Controls.Add(this.JobTextWrite);
+            this.Controls.Add(this.jobTextInput);
+            this.Controls.Add(this.jobNameWrite);
+            this.Controls.Add(this.JobNameInput);
+            this.Name = "ManifestCreator";
             this.Text = "ManifestCreator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private TextBox JobNameInput;
+        private Button jobNameWrite;
+        private TextBox jobTextInput;
+        private Button JobTextWrite;
+        private TextBox stepNameInput;
+        private Button stepNameWrite;
+        private TextBox StepTextInput;
+        private Button StepTextWrite;
+        private Button button5;
+        private TextBox FolderNameText;
+        private Button button6;
+        private Button stepGcodeSelect;
+        private Button WriteStepGcode;
+        private RichTextBox OutputCode;
     }
 }
--- GG3GrblProbe/ManifestCreator.cs
+++ GG3GrblProbe/ManifestCreator.cs
@@ -12,9 +12,169 @@ namespace GG3GrblProbe
 {
     public partial class ManifestCreator : Form
     {
+        public string savepath;
+        public string selectedgcode;
         public ManifestCreator()
         {
             InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            {
+                FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedPath = folderBrowserDialog.SelectedPath;
+                    string folderName = FolderNameText.Text;
+                    string CodeFolderName = "Code";
+
+                    if (!string.IsNullOrEmpty(folderName))
+                    {
+                        string path = Path.Combine(selectedPath, folderName);
+                        string codefolder = Path.Combine(path, CodeFolderName);
+
+                        try
+                        {
+                            Directory.CreateDirectory(path);
+                            Directory.CreateDirectory(codefolder);
+                            savepath = path;
+                            PageDisplay();
+                            MessageBox.Show("Folder created successfully!");
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show($"An error occurred while creating the folder: {ex.Message}");
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a folder name!");
+                    }
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            {
+                FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog();
+
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string selectedPath = folderBrowserDialog.SelectedPath;
+                    savepath = selectedPath;
+                    MessageBox.Show("Folder selected successfully!");
+                    PageDisplay();
+                }
+            }
+        }
+        public void PageDisplay()
+        {
+            button6.Visible = false;
+            FolderNameText.Visible = false;
+            button5.Visible = false;
+
+            JobNameInput.Visible = true;
+            jobNameWrite.Visible = true;
+            jobTextInput.Visible = true;
+            JobTextWrite.Visible = true;
+            stepNameInput.Visible = true;
+            stepNameWrite.Visible = true;
+            StepTextInput.Visible = true;
+            StepTextWrite.Visible = true;
+            stepGcodeSelect.Visible = true;
+            WriteStepGcode.Visible = true;
+
+        }
+
+        private void jobNameWrite_Click(object sender, EventArgs e)
+        {
+            string jobName = JobNameInput.Text;
+            OutputCode.Text += $"- job_name: {jobName}";
+            JobNameInput.Text = "Enter Job Name:";
+            JobNameInput.BackColor = Color.White;
+            jobNameWrite.BackColor = Color.White;
+            jobTextInput.BackColor = Color.Aquamarine;
+            JobTextWrite.BackColor = Color.Aquamarine;
+        }
+
+        private void JobTextWrite_Click(object sender, EventArgs e)
+        {
+            string jobtext = jobTextInput.Text;
+            OutputCode.Text += $"\r\n  job_text: {jobtext}\r\n\r\n  job_steps:\r\n\r\n";
+            jobTextInput.Text = "Enter Job Text:";
+            jobTextInput.BackColor = Color.White;
+            JobTextWrite.BackColor = Color.White;
+            stepNameInput.BackColor = Color.Aquamarine;
+            stepNameWrite.BackColor = Color.Aquamarine;
+        }
+
+        private void stepNameWrite_Click(object sender, EventArgs e)
+        {
+            string stepname = stepNameInput.Text;
+            OutputCode.Text += $"    - step_name: {stepname}";
+            stepNameInput.Text = "Enter Step Name:";
+            stepNameInput.BackColor = Color.White;
+            stepNameWrite.BackColor = Color.White;
+            stepGcodeSelect.BackColor = Color.White;
+            WriteStepGcode.BackColor = Color.White;
+            StepTextInput.BackColor = Color.Aquamarine;
+            StepTextWrite.BackColor = Color.Aquamarine;
+        }
+
+        private void StepTextWrite_Click(object sender, EventArgs e)
+        {
+            string steptext = StepTextInput.Text;
+            OutputCode.Text += $"\r\n      step_text: {steptext}";
+            StepTextInput.Text = "Enter Step Text:";
+            StepTextInput.BackColor = Color.White;
+            StepTextWrite.BackColor = Color.White;
+            stepNameInput.BackColor = Color.Aquamarine;
+            stepNameWrite.BackColor = Color.Aquamarine;
+            stepGcodeSelect.BackColor = Color.Aquamarine;
+            WriteStepGcode.BackColor = Color.Aquamarine;
+        }
+
+        private void stepGcodeSelect_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFilePath = openFileDialog.FileName;
+                selectedgcode = selectedFilePath;
+                MessageBox.Show("File selected successfully!");
+            }
+        }
+
+        private void WriteStepGcode_Click(object sender, EventArgs e)
+        {
+            string sourceFilePath = selectedgcode;
+            string codepath = "Code";
+            string targetFolderPath = Path.Combine(savepath, codepath);
+
+            if (File.Exists(sourceFilePath))
+            {
+                try
+                {
+                    string targetFilePath = Path.Combine(targetFolderPath,sourceFilePath);
+                    File.Move(sourceFilePath, targetFilePath);
+                    MessageBox.Show("File wrote successfully!");
+                    OutputCode.Text += $"\r\n      step_gcode: Code/{Path.GetFileName(sourceFilePath)}\r\n";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred while cutting the file: {ex.Message}");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Selected file does not exist!");
+            }
+            stepGcodeSelect.BackColor = Color.White;
+            WriteStepGcode.BackColor = Color.White;
         }
     }
 }
