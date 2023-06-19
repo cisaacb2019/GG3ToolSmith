6f4e688665f6ecc0e2a6f7cc58685c9ab6ee8873
Collin B <82788579+cisaacb2019@users.noreply.github.com>
2023-06-19 17:08:44 -0600

    Functioning encrypt/decrypt.

---
+++ GG3GrblProbe/AnyCrypt.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace GG3GrblProbe
+{
+    partial class AnyCrypt
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.decrypt = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(117, 101);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(319, 29);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Select file:";
+            this.Start.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // decrypt
+            // 
+            this.decrypt.Location = new System.Drawing.Point(0, 0);
+            this.decrypt.Name = "decrypt";
+            this.decrypt.Size = new System.Drawing.Size(75, 23);
+            this.decrypt.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(107, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Encrypt";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.EncryptButton_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(107, 141);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Decrypt";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.DecryptButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(310, 80);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Below is used to encrypt and decrypt .dd files, notice do not do anything if it i" +
+    "s not allowed in your provedence.";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // AnyCrypt
+            // 
+            this.ClientSize = new System.Drawing.Size(301, 227);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "AnyCrypt";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button Start;
+        private Label label1;
+        private Button decrypt;
+        private Button button1;
+        private Button button2;
+        private Label label2;
+    }
+}
--- GG3GrblProbe/Home.Designer.cs
+++ GG3GrblProbe/Home.Designer.cs
@@ -33,6 +33,7 @@
             this.CheckPoint = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.CommandCenter = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -85,18 +86,30 @@
             this.CommandCenter.UseVisualStyleBackColor = true;
             this.CommandCenter.Click += new System.EventHandler(this.CommandCenter_Click);
             // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(428, 114);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(202, 72);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "AnyCrypt";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
             // Home
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(644, 450);
+            this.ClientSize = new System.Drawing.Size(644, 231);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.CommandCenter);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.CheckPoint);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Home";
-            this.Text = "Form1";
+            this.Text = "Toolsmith";
+            this.Load += new System.EventHandler(this.Home_Load);
             this.ResumeLayout(false);
 
         }
@@ -108,5 +121,6 @@
         private Button CheckPoint;
         private Button button3;
         private Button CommandCenter;
+        private Button button4;
     }
 }
